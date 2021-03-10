d24f9430b12d165d592727853f320830ae5f1f94
Andrew Martins <andrew_omartins@yahoo.com>
2021-03-10 15:01:32 -0500

    creating a backend server for the PERN stack

---
+++ routes/app.js
@@ -0,0 +1,20 @@
+'use strict';
+
+const http = require('http'),
+hostname = '127.0.0.1',
+port = 3001;
+
+const cors = require('cors')
+
+const express = require('express')
+
+const app = express();
+
+app.use(cors());
+
+const server = http.createServer(app);
+
+server.listen(port, hostname, () => {
+    console.log(`Server is running at http://${hostname}:${port}`)
+});
+
